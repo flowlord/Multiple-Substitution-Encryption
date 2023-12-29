4b9e8060e7d49f98f074f5f28967af421ba560c6
FLOW LORD <56801474+flowlord@users.noreply.github.com>
2023-12-30 00:51:09 +0100

    Correction de bug diverse

--- configs/configs_setting.py
+++ configs/configs_setting.py
@@ -6,34 +6,6 @@ from string import ascii_lowercase,ascii_uppercase, ascii_letters, digits, punct
 import json
 import os.path
 
-
-def gen_default_settings():
-	data = """
-{
-        "cipher": "ascii_letters",
-        "special_charac": "easintrluodchEASINTRLUODCH0123456789!#$%&'()*+,-./:;<=>?@[]",
-        "substitue_with": "configs/all.txt",
-        "cipher_punctuation": "True",
-        "cipher_accent": "True",
-        "cipher_digits": "True",
-        "charac_len": [6,9],
-        "len_special_charac": [3,6],
-        "key_number": [3,50],
-        "len_charac_group_b":[4,10],
-        "mini_add_group_b_charac": 24,
-        "max_add_group_b_charac": 300
-}
-"""
-	settings_file = open("configs/setting.json", "w", encoding="utf-8")
-
-	settings_file.write(data)
-
-	settings_file.close()
-
-if os.path.exists("configs/setting.json") is False:
-	gen_default_settings()
-
-
 accent = "ÄÀÂÉÈÊËÎÏÔÙÛÜÇàâéèêëîïôùûüç"
 
 data = json.load(open("configs/setting.json", "r"))
